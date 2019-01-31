2e549de0d7ec5dd7de13069d91c913cbc394896d
Yusuke Izawa <yuizalp@gmail.com>
2019-01-31 16:44:09 +0900

    mincaml_part.ml -> mincaml_part.mc.ml

---
+++ ocaml-c-mincaml/mincaml_part.mc.ml
@@ -0,0 +1,4 @@
+let res1 = call_caml_fib 30 in
+let res2 = call_caml_add 100 200 in 
+print_int res1; print_newline ();
+print_int res2
--- ocaml-c-mincaml/mincaml_part.ml
+++
@@ -1,2 +0,0 @@
-let res = call_caml_fib 10 in
-print_int res; print_newline ()
