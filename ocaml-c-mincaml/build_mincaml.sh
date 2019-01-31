32894ab8736d6cde6153010191ac71103b251af6
Yusuke Izawa <yuizalp@gmail.com>
2019-01-31 16:47:29 +0900

    update build_mincaml.sh

--- ocaml-c-mincaml/build_mincaml.sh
+++ ocaml-c-mincaml/build_mincaml.sh
@@ -3,11 +3,15 @@ MINCAML=~/src/github.com/esumii/min-caml
 
 gcc -m32 -c -I"`ocamlc -where`" c_part.c -o c_part.o
 
+gcc -m32 -c ml_part_interop.s -o c_part_interop.o
+
 ocamlopt -output-obj -o ml_part.o unix.cmxa ml_part.ml
 
-gcc -m32 -c -I"$(ocamlc -where)" mincaml_part.s -o mincaml_part.o
+min-caml mincaml_part.mc
+
+gcc -m32 -c -I"$(ocamlc -where)" mincaml_part.mc.s -o mincaml_part.mc.o
 
 gcc -m32 -o mincaml_part -I"$(ocamlc -where)" -L"$(ocamlc -where)" -lunix -lasmrun -lcurses \
-		c_part.o ml_part.o mincaml_part.o \
+		c_part.o ml_part.o mincaml_part.mc.o ml_part_interop.o \
 		~/src/github.com/esumii/min-caml/libmincaml.S \
 		~/src/github.com/esumii/min-caml/stub.c
