8ee624568d4ace6940857f5104eac54f30d39579
Yusuke Izawa <yuizalp@gmail.com>
2020-06-23 04:14:34 +0900

    Updated.

--- dylink/asm-c-ocaml/interop.c
+++ dylink/asm-c-ocaml/interop.c
@@ -1,4 +1,3 @@
-#include <caml/compatibility.h>
 #include <caml/alloc.h>
 #include <caml/callback.h>
 #include <caml/mlvalues.h>
@@ -6,6 +5,13 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <caml/mlvalues.h>
+#include <caml/callback.h>
+
+
+extern void call_caml_jit_merge_point(int *, int, int *, int);
 
 value init_f(int n) { return Val_int(n); }
 
@@ -15,12 +21,12 @@ void call_caml_jit_merge_point(int *stk, int sp, int *bytecode, int pc) {
   if (clsr == NULL) {
     clsr = caml_named_value("jit_merge_point");
   }
-  ml_args[0] = caml_alloc_array(init_f, bc);
-  ml_args[1] = caml_alloc_array(init_f, st);
+  ml_args[0] = caml_alloc_array(init_f, bytecode);
+  ml_args[1] = caml_alloc_array(init_f, stk);
   ml_args[2] = Val_int(pc);
   ml_args[3] = Val_int(sp);
-  ml_args[4] = Val_hp(bc);
-  ml_args[5] = Val_hp(st);
+  ml_args[4] = Val_hp(bytecode);
+  ml_args[5] = Val_hp(stk);
   caml_callbackN(*clsr, 6, ml_args);
   return;
 }
