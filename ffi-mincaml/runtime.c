be64fcb0d8e6fd2f0d1ed7f87c23baf3738266c7
Yusuke Izawa <yuizalp@gmail.com>
2020-06-24 13:20:58 +0900

    Updated.

--- ffi-mincaml/runtime.c
+++ ffi-mincaml/runtime.c
@@ -1,13 +1,12 @@
 #define _GNU_SOURCE
 
 #include <assert.h>
-#define CAML_NAME_SPACE
 #include <caml/alloc.h>
-#include <caml/mlvalues.h>
-#include <caml/memory.h>
-#include <caml/custom.h>
 #include <caml/callback.h>
 #include <caml/compatibility.h>
+#include <caml/custom.h>
+#include <caml/memory.h>
+#include <caml/mlvalues.h>
 #include <dlfcn.h>
 #include <signal.h>
 #include <stdbool.h>
@@ -16,7 +15,6 @@
 #include <string.h>
 #include <sys/time.h>
 #include <unistd.h>
-#include <uthash.h>
 
 #include "modwrap.h"
 
@@ -56,112 +54,27 @@ void gen_so_name(char *buffer, char *tname) {
   sprintf(buffer, "./lib%s.so", tname);
 }
 
-struct prof {
-  int pc;             // key
-  int count;          // value 1
-  char so_name[1024]; // value 2
-};
-
-struct prof *prof_tbl[PROF_LEN] = {NULL};
-
-int prof_tbl_cnt = 0;
-
-void insert_prof(int pc, int count, char *so_name) {
-  for (int i = 0; i < PROF_LEN; i++) {
-    if (prof_tbl[i] == NULL) {
-      struct prof *p;
-      p = malloc(sizeof(struct prof));
-      p->pc = pc;
-      p->count = count;
-      strcpy(p->so_name, so_name);
-      prof_tbl[i] = p;
-      return;
-    }
-  }
-}
-
-struct prof *find_prof(int pc) {
-  for (int i = 0; i < PROF_LEN; i++) {
-    struct prof *p = prof_tbl[i];
-    if (p && p->pc == pc) {
-      return p;
-    }
-  }
-  return NULL;
-}
-
-struct sym {
-  char filename[1024]; // key
-  char funcname[1024]; // value 1
-  fun_arg2 sym;        // value 2
-  UT_hash_handle hh;
-};
-
-struct sym_pc {
-  int pc;
-  void *handle;
-  fun_arg2 sym;
-  UT_hash_handle hh;
-};
-
-struct sym *syms = NULL;
-
-struct sym_pc *sym_pcs = NULL;
-
-void add_sym(char *filename, char *funcname, fun_arg2 sym) {
-  struct sym *s;
-  s = malloc(sizeof(struct sym));
-  strcpy(s->filename, filename);
-  strcpy(s->funcname, funcname);
-  s->sym = sym;
-  HASH_ADD_STR(syms, filename, s);
-}
-
-void add_sym_pc(int pc, void *handle, fun_arg2 sym) {
-  struct sym_pc *s;
-  HASH_FIND_INT(sym_pcs, &pc, s);
-  if (s == NULL) {
-    s = malloc(sizeof(struct sym_pc));
-    HASH_ADD_INT(sym_pcs, pc, s);
-  }
-  s->pc = pc;
-  s->handle = handle;
-  s->sym = sym;
-}
-
-struct sym *find_sym(char *funcname) {
-  struct sym *s;
-
-  HASH_FIND_STR(syms, funcname, s);
-  return s;
-}
-
-struct sym_pc *find_sym_pc(int pc) {
-  struct sym_pc *s;
-  HASH_FIND_INT(sym_pcs, &pc, s);
-
-  return s;
-}
+int prof_arr[2048] = {0};
 
 fun_arg2 sym_arr[2048] = {NULL};
 
 fun_arg2 call_dlfun_arg2(char *filename, char *funcname, int pc, int arg1,
-                    int arg2) {
+                         int arg2) {
   fun_arg2 sym = NULL;
   void *handle = NULL;
   int res;
 
   handle = dlopen(filename, RTLD_LAZY | RTLD_GLOBAL);
   if (handle == NULL) {
-	fprintf(stderr, "error: dlopen %s\n", filename);
-	return NULL;
+    fprintf(stderr, "error: dlopen %s\n", filename);
+    return NULL;
   }
   dlerror();
 
   sym = (fun_arg2)dlsym(RTLD_DEFAULT, funcname);
   if (sym == NULL) {
-	fprintf(stderr, "error: dlsym \n");
-	return NULL;
+    fprintf(stderr, "error: dlsym \n");
+    return NULL;
   }
   sym_arr[pc] = sym;
   printf("added sym:\t%s\t%d\n", filename, pc);
@@ -198,16 +111,19 @@ void call_jit_merge_point(int *stack, int sp, int *code, int pc) {
     strcpy(func, "sub");
 
   sym = sym_arr[pc];
-  if (sym == NULL) {
-    gen_trace_name(tname, TJ);
-    gen_so_name(so, tname);
-    jit_compile(so, func, pc);
-    insert_prof(pc, 0, so);
-    sym = call_dlfun_arg2(so, func, pc, 3, 3);
-  } else {
-	printf("found sym\n");
+  prof_arr[pc]++;
+
+  if (prof_arr[pc] > 1) {
+    if (sym == NULL) {
+      gen_trace_name(tname, TJ);
+      gen_so_name(so, tname);
+      jit_compile(so, func, pc);
+      sym = call_dlfun_arg2(so, func, pc, 3, 3);
+    } else {
+      printf("found sym\n");
+    }
+    res = sym(3, 3);
+    printf("result: %d\n", res);
   }
-  res = sym(3, 3);
-  printf("result: %d\n", res);
   return;
 }
